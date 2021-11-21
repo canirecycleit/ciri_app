1946feb04a551d69927dd40edb21ea566af4430b
Kevin Hill <hill.ck@pg.com>
2021-11-21 19:34:30 +0800

    persistent disk usage.

--- k8s_deployment/create_cluster.sh
+++ k8s_deployment/create_cluster.sh
@@ -1,10 +1,11 @@
 gcloud container clusters create ciri-k8s-cluster --num-nodes 2 --zone us-east1-c
 
+# Install nginx-ingress
 helm upgrade --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress-nginx --create-namespace
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
 
-# kubectl create ingress ui --class=nginx --rule=/*=ui:8080
-# kubectl create ingress api --class=nginx --rule=/api/*=api:8080
+# Create persistent disk
+gcloud compute disks create --size=10GB --zone=us-east1-c gce-db-disk
