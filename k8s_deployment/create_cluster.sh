d495cc82b8bd38ff8e97f5a829235de770ddf02e
Kevin Hill <hill.ck@pg.com>
2021-11-26 10:47:08 +0800

    Refactored with vars.

--- k8s_deployment/create_cluster.sh
+++ k8s_deployment/create_cluster.sh
@@ -1,4 +1,6 @@
-gcloud container clusters create ciri-k8s-cluster --num-nodes 2 --zone us-east1-c
+gcloud container clusters create $GCP_CLUSTER_NAME --num-nodes 2 --zone $GCP_ZONE
+
+gcloud container clusters get-credentials --zone $GCP_ZONE $GCP_CLUSTER_NAME
 
 # Install nginx-ingress
 helm upgrade --install ingress-nginx ingress-nginx \
@@ -7,5 +9,6 @@ helm upgrade --install ingress-nginx ingress-nginx \
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
 
+
 # Create persistent disk
-gcloud compute disks create --size=10GB --zone=us-east1-c gce-db-disk
+gcloud compute disks create --size=10GB --zone=$GCP_ZONE gce-db-disk
